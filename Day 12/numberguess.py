b2504f327ac91616f8843926ee1481bdca9643b5
Vishnu Narayan V <vichuroxx@gmail.com>
2021-01-31 23:49:15 +0530

    Update numberguess.py

--- Day 12/numberguess.py
+++ Day 12/numberguess.py
@@ -2,6 +2,7 @@ import random
 actual=random.randint(1,100)
 flag=True
 count=0
+mode=input("Select mode :- Easy or difficult (E/D)")
 while flag is True:
         count+=1
         guess=int(input("Guess a Number between 1-100 :- "))
@@ -12,9 +13,16 @@ while flag is True:
             flag=False
         else:
             print("Guessed Number Low,Try High")
-        print("You Have "+str(10-count)+" Chances Left")
-        if(count==10):
-            print("Game Over")
-            flag=False
+        if mode=='D':
+            print("You Have "+str(5-count)+" Chances Left")
+            if(count==5):
+                print("Game Over")
+                flag=False
+        elif mode=='E':
+            print("You Have "+str(10-count)+" Chances Left")
+            if(count==10):
+                print("Game Over")
+                flag=False
+        
         
     
